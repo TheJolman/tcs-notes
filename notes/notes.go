ad70dd0a1b514bcf447930168160ba4533ab90f3
Josh Holman <git@jolman.me>
2026-05-09 12:48:27 -0700

    template funcs

--- notes/notes.go
+++ notes/notes.go
@@ -27,15 +27,36 @@ func WriteNoteAndHW(studentName string, date string) error {
 		return fmt.Errorf("failed to create note: %v", err)
 	}
 	// Write homework
-	hwPath := path.Join(studentDateDir, "hw.md")
+	hwPath := path.Join(studentDateDir, "hw.txt")
 	if err := writeFile(hwPath); err != nil {
 		return fmt.Errorf("failed to create hemework: %v", err)
 	}
 	return nil
 }
 
-func MakeNoteTemplate() {
-	_ = configDir
+func WriteNoteTemplate() error {
+	return writeTemplateFile("note")
+}
+
+func WriteHWTemplate() error {
+	return writeTemplateFile("hw")
+}
+
+// param `template` should be one of "note", "hw"
+func writeTemplateFile(templateType string) error {
+	if templateType != "note" && templateType != "hw" {
+		panic("Error: `templateType` argument must be one of \"note\", \"hw\"")
+	}
+
+	if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create config directory: %v", err)
+	}
+
+	templatePath := path.Join(configDir, fmt.Sprintf("%s_template.md", templateType))
+	if err := writeFile(templatePath); err != nil {
+		return fmt.Errorf("failed to create %s template: %v", templateType, err)
+	}
+	return nil
 }
 
 func writeFile(filepath string) error {
