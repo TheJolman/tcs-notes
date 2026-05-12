096228caa67d1419075d21284fa465456ef6033e
Josh Holman <git@jolman.me>
2026-05-12 15:14:17 -0700

    don't use helper function for writing template files

--- notes/notes.go
+++ notes/notes.go
@@ -35,26 +35,25 @@ func WriteNoteAndHW(studentName string, date string) error {
 }
 
 func WriteNoteTemplate() error {
-	return writeTemplateFile("note")
-}
-
-func WriteHWTemplate() error {
-	return writeTemplateFile("hw")
-}
-
-// param `template` should be one of "note", "hw"
-func writeTemplateFile(templateType string) error {
-	if templateType != "note" && templateType != "hw" {
-		panic("Error: `templateType` argument must be one of \"note\", \"hw\"")
-	}
-
 	if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
 		return fmt.Errorf("failed to create config directory: %v", err)
 	}
 
-	templatePath := path.Join(configDir, fmt.Sprintf("%s_template.md", templateType))
+	templatePath := path.Join(configDir, "note_template.md")
 	if err := writeFile(templatePath); err != nil {
-		return fmt.Errorf("failed to create %s template: %v", templateType, err)
+		return fmt.Errorf("failed to create note template: %v", err)
+	}
+	return nil
+}
+
+func WriteHWTemplate() error {
+	if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create config directory: %v", err)
+	}
+
+	templatePath := path.Join(configDir, "hw_template.txt")
+	if err := writeFile(templatePath); err != nil {
+		return fmt.Errorf("failed to create hw template: %v", err)
 	}
 	return nil
 }
